d2f0319a37be4646e08acd58a8d19fd51fccaa8a
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2020-10-12 14:52:24 +0200

    Create row

--- Program.cs
+++ Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace categoryReaderRO
 {
     class Program
     {
+        const string pathToFile = @"C:\Users\walkowskip\Downloads\test1.xlsx";
+        const string scriptFileName = "categoryReaderRO_script.sql";
+
         static void Main(string[] args)
         {
             Console.WriteLine("categoryReaderRO v1.0");
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var model = ReadFile();
+
+        }
+
+        private static HashSet<Row> ReadFile()
+        {
+            using var stream = File.Open(pathToFile, FileMode.Open, FileAccess.Read);
+            using var reader = ExcelReaderFactory.CreateReader(stream);
+            var rowList = new HashSet<Row>();
+
+            do
+            {
+                reader.Read();
+
+                while (reader.Read())
+                {
+                    var row = new Row();
+                    row.Category = reader[3].ToString();
+
+                    if (rowList.Contains(row,))
+                    {
+                        rowList.TryGetValue(row, out Row actualRow);
+                        SetValue(actualRow, reader);
+                    }
+
+                    rowList.Add(SetValue(row, reader));
+
+                }
+            } while (reader.NextResult());
+
+            return rowList;
+        }
+
+        private static Row SetValue(Row actualRow, IExcelDataReader reader)
+        {
+            if (actualRow.Id == Guid.Empty)
+            {
+                actualRow.Id = Guid.NewGuid();
+            }
+
+            actualRow.WGR = reader[1].ToString();
+            actualRow.WUGR = reader[2].ToString().Split(',').ToList();
+
+            return actualRow;
         }
     }
 }
---
+++ Row.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace categoryReaderRO
+{
+    internal class Row : IEquatable<Row>
+    {
+        public Row()
+        {
+        }
+
+        public string Category { get; internal set; }
+        public Guid Id { get; internal set; }
+        public string WGR { get; internal set; }
+        public List<string> WUGR { get; internal set; }
+
+        public bool Equals([AllowNull] Row other)
+        {
+            return Category == other.Category;
+        }
+    }
+}
