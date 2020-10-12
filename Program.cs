93e9e7644d75714bf79ff7a96e0f22773092133b
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2020-10-12 14:09:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace categoryReaderRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("categoryReaderRO v1.0");
+        }
+    }
+}
